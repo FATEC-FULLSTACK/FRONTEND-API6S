9180517f5fe0cdae0d6695be2c4c8909673ffebd
giovani <giovanicarvalhoavila@hotmail.com>
2025-03-20 12:40:14 -0300

    feat: add initial homepage

---
+++ Client/src/components/ChatInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <input
+    type="text"
+    placeholder="Digite sua pergunta..."
+    class="w-full bg-[#2b2b2b] h-[65px] placeholder:text-sm p-6 text-sm text-white rounded-[15px] outline-none"
+  />
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ChatInput',
+}
+</script>
---
+++ Client/src/pages/HomePage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="grid justify-center h-screen">
+    <main class="flex flex-col justify-between mt-[64px] w-[800px]">
+      <section class="flex flex-col items-center">
+        <img src="../assets/logo/logo.svg" alt="IA Logo" width="30px" height="30px" />
+        <div class="py-5">
+          <h2 class="text-3xl font-medium text-white-600 text-center break-words">Hello, User</h2>
+          <h1
+            class="text-4xl mt-2 font-semibold gradient-bright-effect bg-gradient-to-r from-green-400 via-teal-500 to-sky-400 bg-clip-text text-transparent"
+          >
+            Como posso ajudar você hoje?
+          </h1>
+        </div>
+        <p class="text-xs max-w-[455px] text-center leading-[27px] typing-text"></p>
+      </section>
+      <section class="w-full mb-[128px]">
+        <ChatInput />
+      </section>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+// @ts-expect-error it is working this effect
+import Typewriter from 'typewriter-effect/dist/core'
+import ChatInput from '../components/ChatInput.vue'
+
+export default defineComponent({
+  name: 'MainComponent',
+  components: {
+    ChatInput,
+  },
+  setup() {
+    onMounted(() => {
+      const typingText = document.querySelector<HTMLElement>('.typing-text')
+      if (typingText) {
+        new Typewriter(typingText, {
+          strings: [
+            'Este é um sistema de IA desenvolvido para avaliar critérios e treinar respostas de forma eficaz.',
+          ],
+          autoStart: true,
+          loop: true, 
+          delay: 15, 
+          cursor: '|', 
+          pauseFor: '450000',
+        })
+      }
+    })
+  },
+})
+</script>
