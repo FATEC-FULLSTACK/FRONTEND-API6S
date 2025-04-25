0676944283d7c4a057a50cc382989b8aff39ade0
Carlos-Henrique39 <ique.benicio@hotmail.com>
2025-04-25 16:16:31 -0300

    FAT-35: feature: Pagina de Criterios de Avaliação

--- Client/src/router/index.ts
+++ Client/src/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomePage,
     },
+    {
+      path: '/avaliacao',
+      name: 'criterios',
+      component: Criterios,
+    }
   ],
 })
 
